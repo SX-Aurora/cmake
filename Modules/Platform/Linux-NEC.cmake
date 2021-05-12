cb1b75138f1e8c439b11a45a4a08f1c0d6f73a02
Fredrik Unger <fred@tree.se>
2021-05-12 10:38:15 +0200

    NEC: Flag and Library search path adjustment
    Patch provided by LG/NEC

--- Modules/Platform/Linux-NEC.cmake
+++ Modules/Platform/Linux-NEC.cmake
@@ -5,6 +5,10 @@ set(__LINUX_COMPILER_NEC 1)
 
 macro(__linux_compiler_nec lang)
   set(CMAKE_${lang}_COMPILE_OPTIONS_PIC "-fPIC")
+  set(CMAKE_${lang}_COMPILE_OPTIONS_PIE "") # -fPIE is not supported and ignored by NEC compiler
   set(CMAKE_SHARED_LIBRARY_${lang}_FLAGS "-fPIC")
   set(CMAKE_SHARED_LIBRARY_CREATE_${lang}_FLAGS "-shared")
 endmacro()
+
+# Will be used by find_library as search path
+list(APPEND CMAKE_PREFIX_PATH /opt/nec/ve/lib)
